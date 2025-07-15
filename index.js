87a6d26380cedf006f810a2886244084a5b15eae
Hanzala Habib <imhanzala444@gmail.com>
2025-07-15 14:30:47 +0500

    use middleware to access req.body data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { connectToDb } =require('./connection');
 const app= express();
 const PORT=8001;
 connectToDb('mongodb://localhost:27017/short_url').then(() => console.log("mongodb connect successfully") );
-
+app.use(express.json())
 app.use('/',urlRoute);
 
 app.listen(PORT,()=>console.log(`server started at port: ${PORT}`))
